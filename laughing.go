c1f76ae626c236a0a2fe8e24d0d9c11de42132dd
Todd Hunter <todd.hunter@gmail.com>
2019-05-03 08:41:12 +1000

    Addition of Kinesis libraries.

--- laughing.go
+++ laughing.go
@@ -7,6 +7,11 @@ import (
 	"fmt"
 	"net/http"
 	"flag"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+
 )
 
 const zKillboardURL = "https://redisq.zkillboard.com/listen.php"
@@ -15,13 +20,28 @@ const zKillboardURL = "https://redisq.zkillboard.com/listen.php"
 var (
 	tee_file *os.File
 	err error
+
+	tee_filename = flag.String("s","","Tee output to givenfilename")
+	append_tee   = flag.Bool("a", false, "Append to existing output (if not given, -s will overwrite file")
+	stream_name  = flag.String("ks","killboard","Name of kinesis stream to push to")
+
+
 )
 
 func main() {
 
-	tee_filename := flag.String("s","","Tee output to givenfilename")
-	append_tee := flag.Bool("a", false, "Append to existing output (if not given, -s will overwrite file")
+	aws_session := session.Must(session.NewSession())
 	
+	kc := kinesis.New(aws_session)
+
+	streams, err := kc.DescribeStream(&kinesis.DescribeStreamInput{StreamName: stream_name})
+	if err != nil {
+		panic(err)
+	}
+  fmt.Printf("%v\n", streams)
+	
+	
+
 	flag.Parse()
 
 	write_output := false
@@ -34,15 +54,16 @@ func main() {
 		if *append_tee {
 			file_mode = os.O_APPEND|os.O_CREATE|os.O_WRONLY
 		}
-		fmt.Println("file_mode = %d", file_mode)
+		fmt.Println("file_mode = ", file_mode)
 		tee_file, err = os.OpenFile(*tee_filename, file_mode, 0644)
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer tee_file.Close()
 	}
 
 	i := 0
-	for true {
+	for true {  // Main loop
 		resp, err := http.Get(zKillboardURL)
 		if err != nil {
 			log.Fatal(err)
@@ -62,6 +83,17 @@ func main() {
 			fmt.Printf("Empty result, restarting")
 			continue
 		}
+
+		putOutput, err := kc.PutRecord(&kinesis.PutRecordInput{
+			Data: body,
+			StreamName: stream_name,
+			PartitionKey: aws.String("kill"),
+		})
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%v\n", putOutput)
 
 		// fmt.Printf("%d %d Processed JSON: %#v", i,  killmail.Package.KillId, killmail)
 		if write_output {
